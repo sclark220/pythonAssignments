da56426a63e02213fb2fcdea7c4d78136d1fd0f9
sclark220 <113330749+sclark220@users.noreply.github.com>
2023-11-05 21:51:28 -0500

    Update Chapter3.py
    Added Two (2) prints.

--- BookAssignments/M01/Chapter3.py
+++ BookAssignments/M01/Chapter3.py
@@ -1,6 +1,9 @@
 secondsPerHour = 60 * 60
 secondsPerDay = secondsPerHour * 24
 
+#Is this it?
+print(secondPerHour)
+print(secondPerDay)
 # float division
 print(secondsPerDay / secondsPerHour)
 # integer division
